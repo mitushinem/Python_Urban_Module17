4f7ecee92ba75531f5bb84e375cc9ceff75b2486
Evgeny M. Mitushin <mitushinem@gmail.com>
2024-11-10 11:58:45 +0300

    Домашнее задание по теме "Использование БД в маршрутизации. Часть 1.

--- routers/user.py
+++ routers/user.py
@@ -4,39 +4,43 @@ from sqlalchemy import insert, update, select, delete
 from sqlalchemy.orm import Session
 from backend.db_depends import get_db
 from typing import Annotated
-from models.user import User
 
+from models.user import User
 from schemas import CreateUser, UpdateUser
 
 router_user = APIRouter(prefix="/user", tags=["user"])
 
+
 @router_user.get("/")
 async def all_users(db: Annotated[Session, Depends(get_db)]):
-    users = db.execute(select(User)).scalars().all()
-    return users
+    users = db.scalars(select(User))
+    return users.all()
 
 
 @router_user.get("/user_id")
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
-    user = db.execute(select(User).where(User.id == user_id))
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
-
     return user
 
 
 @router_user.post("/create")
 async def create_user(db: Annotated[Session, Depends(get_db)], user: CreateUser):
-    # user_by = db.scalars(select(User).where(User.username == user.username)).all()
-    # if user_by is not None:
-    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
+    new_user = db.scalar(select(User).where(User.username == user.username))
+    if new_user is None:
+        db.execute(insert(User).values(username=user.username,
+                                       lastname=user.lastname,
+                                       firstname=user.firstname,
+                                       age=user.age,
+                                       slug=slugify(user.username)))
+        db.commit()
+    else:
+        return {
+            'status_code': status.HTTP_404_NOT_FOUND,
+            'transaction': 'The user is already in the database',
+        }
 
-    db.execute(insert(User).values(username=user.username,
-                                   lastname=user.lastname,
-                                   firstname=user.firstname,
-                                   age=user.age,
-                                   slug=slugify(user.username)))
-    db.commit()
     return {
         'status_code': status.HTTP_201_CREATED,
         'transaction': 'User created successfully',
@@ -45,7 +49,7 @@ async def create_user(db: Annotated[Session, Depends(get_db)], user: CreateUser)
 
 @router_user.put("/update")
 async def update_user(db: Annotated[Session, Depends(get_db)], user: UpdateUser, user_id: int):
-    user_by = db.execute(select(User).where(User.id == user_id))
+    user_by = db.scalar(select(User).where(User.id == user_id))
     if user_by is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
 
